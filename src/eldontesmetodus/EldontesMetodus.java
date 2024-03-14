4cf04283c06e272c6fc34ae7460230167a5dfa6d
lajkogege <lajkogege@gmail.com>
2024-03-14 11:31:38 +0100

    eldontes tetele hibas

---
+++ src/eldontesmetodus/EldontesMetodus.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eldontesmetodus;
+
+/**
+ *
+ * @author LajkóGergő(SZOFT_202
+ */
+public class EldontesMetodus {
+
+    public static void main(String[] args) {
+        primSzam();
+    }
+
+    private static void primSzam() {
+        int[] tomb =tomb();
+        boolean van= eldontesTetele(tomb);
+        tombKiiratas(tomb);
+        kiIratás(van);
+    }
+
+    private static int[] tomb() {
+      int [] szamokTomb={12,34,11,222,445,66,3,5,1,7};
+      return szamokTomb;
+    }
+
+    private static boolean eldontesTetele(int[] tomb) {
+       boolean van =false;
+       int primszam=0;
+       for (int i = 0; i < tomb.length; i++) {
+            int szam = tomb[i];
+            int oszto = 1;
+            while (oszto < szam && !van) {
+                if (szam % oszto != 0) {
+                    van=true;
+                    System.out.println(tomb[i]);
+                }
+                oszto++;
+            }
+        }
+        return van;
+    }
+    
+    private static void kiIratás(boolean van) {
+         if (van==true ) {
+             System.out.println("Van benne prim");
+        }
+        else{
+            System.out.println("Nincs benne prim");
+        }
+    }
+
+    private static void tombKiiratas(int[] tomb) {
+        for (int i = 0; i < tomb.length; i++) {
+            if (i==tomb.length-1) {
+                System.out.print(tomb[i] + "");
+            }
+            else{
+                System.out.print(tomb[i] + ";");
+            }
+        }
+        System.out.println("");
+    }
+        
+}
+
+    
